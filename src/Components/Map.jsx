84bb3d66e6d93f5585adf3b5033546b727b1fe68
Maria Mantsurova <marimantsurova@gmail.com>
2023-03-19 23:51:57 +0200

    map changes

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,17 +1,18 @@
 import Map from 'ol/Map.js'
 import TileJSON from 'ol/source/TileJSON.js'
 import { fromLonLat } from 'ol/proj'
+import OSM from 'ol/source/OSM'
 import TileLayer from 'ol/layer/Tile.js'
 import View from 'ol/View.js'
-import React, { useEffect, useMemo, useState } from 'react'
-import Styles from '../styles/map.module.scss'
-function MapView() {
-  const [map, setMap] = useState(undefined)
-  const mapSetting = useMemo(() => {
-    console.log('render')
-    setMap(
-      new Map({
-        target: 'map',
+import React, { useEffect, useRef } from 'react'
+
+function MapView({ zoom = 1 }) {
+  const ref = useRef(null)
+  const mapRef = useRef(null)
+  useEffect(() => {
+    console.log("I'm mounting!")
+    if (ref.current && !mapRef.current) {
+      mapRef.current = new Map({
         layers: [
           new TileLayer({
             source: new TileJSON({
@@ -21,18 +22,36 @@ function MapView() {
             }),
           }),
         ],
-        view: new View({
-          center: fromLonLat([34.989571, 32.794044]),
-          zoom: 2,
-        }),
-      }),
-    )
-  }, [])
+        view: new View({ center: [0, 0], zoom: 1 }),
+        target: ref.current,
+      })
+    }
+  }, [ref, mapRef])
 
   useEffect(() => {
-    mapSetting
-  }, [])
-  return <div id="map" className={Styles['map']}></div>
+    mapRef.current?.getView().setZoom(zoom)
+  }, [mapRef, zoom])
+
+  return <div ref={ref} style={{ width: '100%', height: '100%' }} />
 }
 
 export default MapView
+
+//   useEffect(() => {
+//     if (map === undefined) {
+//       setMap(
+//         new Map({
+//           target: map,
+//           layers: [
+//             new TileLayer({
+
+//             }),
+//           ],
+//           view: new View({
+//             center: fromLonLat([34.989571, 32.794044]),
+//             zoom: 2,
+//           }),
+//         }),
+//       )
+//     }
+//   }, [])
