bf05507249615bdd5aa8ff21e43ebdaa5863911a
Maria Mantsurova <marimantsurova@gmail.com>
2023-03-30 17:10:26 +0300

    new config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['fontfaceobserver'],
+      external: ['/fontfaceobserver', /node_modules/],
     },
   },
 })
