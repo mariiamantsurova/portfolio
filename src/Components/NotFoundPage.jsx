e82ebeb9fd6964aed895b05aaf197f4ec805322a
Maria Mantsurova <marimantsurova@gmail.com>
2023-03-30 13:09:13 +0300

    not found page 404 page

--- src/Components/NotFoundPage.jsx
+++ src/Components/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Styles from '../styles/notfoundpage.module.scss'
+import Styles from '../styles/modal.module.scss'
 import { Link } from 'react-router-dom'
 import sadIcon from '/icons/notFoundPage/disapointicon1.svg'
 import sadIcon2 from '/icons/notFoundPage/disapointicon2.svg'
@@ -11,21 +11,26 @@ function NotFoundPage() {
     { src: sadIcon3, style: 'icon3' },
   ]
   return (
-    <section className={Styles['section-notFound']}>
+    <section className={Styles['section']}>
       <div className={Styles['container']}>
-        <div className={Styles['title-not-found']}>
+        <div className={Styles['title']}>
           <h1>404</h1>
           <h2>404</h2>
         </div>
-        <h3>
-          The page was not found
-          <br />
-          <Link to="/">Go back to website</Link>
-        </h3>
-        {icons.map(({ src, style }) => (
-          <img key={style} src={src} className={`${Styles[style]}`} />
-        ))}
+        <div className={Styles['text-wrapper']}>
+          <h3>The page was not found</h3>
+          <div className={Styles['Link-wrapper']}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <Link to="/">Go back to website</Link>
+          </div>
+        </div>
       </div>
+      {icons.map(({ src, style }) => (
+        <img key={style} src={src} className={`${Styles[style]}`} />
+      ))}
     </section>
   )
 }
--- src/Components/ThankYou.jsx
+++ src/Components/ThankYou.jsx
@@ -1,7 +1,43 @@
-import React from "react";
-
+import React from 'react'
+import Styles from '../styles/modal.module.scss'
+import { Link } from 'react-router-dom'
+import happyIcon1 from '/icons/ThankYou/happyicon1.svg'
+import happyicon2 from '/icons/ThankYou/happyicon2.svg'
+import happyicon3 from '/icons/ThankYou/happyicon3.svg'
 function ThankYou() {
-	return <div>ThankYou</div>;
+  const icons = [
+    { src: happyIcon1, style: 'icon1' },
+    { src: happyicon2, style: 'icon2' },
+    { src: happyicon3, style: 'icon3' },
+  ]
+  return (
+    <section className={Styles['section']}>
+      <div
+        className={Styles['container']}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '5rem',
+        }}
+      >
+        <div className={Styles['title']}>
+          <h1>Thank You</h1>
+          <h2>Thank You</h2>
+        </div>
+        <div className={Styles['Link-wrapper']}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <Link to="/">Go back to website</Link>
+        </div>
+      </div>
+      {icons.map(({ src, style }) => (
+        <img key={style} src={src} className={`${Styles[style]}`} />
+      ))}
+    </section>
+  )
 }
 
-export default ThankYou;
+export default ThankYou
